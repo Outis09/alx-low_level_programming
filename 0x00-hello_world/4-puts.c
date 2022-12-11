b27ac7f6801f0274a4cbea35459864e4007a0b34
Samuel Ayer <ayersamuel07@gmail.com>
2022-12-11 11:12:58 -0800

    Add semi-colon and use betty style
    ALX checker failed to compile code because there was no semi-colon after
    the return() function so this commit add a semi-colon to the function
    
    The main function was not well documented so this commit adds a
    description to function using the betty documentation guide provided by
    the Holberton School

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-int main(void)
+/**
+ * main - uses puts to print the value stored in the mystr variable
+ */
+int main()
 {
 	char mystr[] = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(mystr);
 
-	return(0)
+	return (0);
 }
