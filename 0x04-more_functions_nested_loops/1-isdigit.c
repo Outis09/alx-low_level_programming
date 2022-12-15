777d1f36ba8b9d6ee123e774089ccf8feddb521a
Samuel Ayer <ayersamuel07@gmail.com>
2022-12-15 15:05:32 -0800

    Change formula

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <ctype.h>
 #include "main.h"
 
 /**
@@ -11,7 +10,7 @@
 
 int _isdigit(int c)
 {
-	if (isdigit(c))
+	if (c >= '0' && c <= '9')
 		return (1);
 
 	return (0);
