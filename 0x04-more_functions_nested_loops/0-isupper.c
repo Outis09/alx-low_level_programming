9fa1a29a887699f4792d806328c80b0edcfd270d
Samuel Ayer <ayersamuel07@gmail.com>
2022-12-15 14:33:49 -0800

    Remove trailing whitespace

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <ctype.h>
+#include "main.h"
 
-/* 
+/**
  * _isupper - checks if a character is uppercase
+ * @c: int type number
  * Return: 1 if character is uppercase, otherwise 0
  */
 
@@ -10,6 +12,6 @@ int _isupper(int c)
 {
 	if (isupper(c))
 		return (1);
-	
+
 	return (0);
 }
