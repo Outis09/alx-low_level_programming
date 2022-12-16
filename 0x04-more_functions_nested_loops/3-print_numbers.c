1ebc2748bd0cd59f7b20e9ef62fc84394beade1d
Samuel Ayer <ayersamuel07@gmail.com>
2022-12-15 16:26:42 -0800

    Create file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Return: numbers between 0 and 9
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar("%d\n", 1);
+		i++;
+	}
+
+	return (void);
+}
