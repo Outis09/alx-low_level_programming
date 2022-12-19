6670b66b68d1588e97f05ab70d0330fbf6d52b2d
Samuel Ayer <ayersamuel07@gmail.com>
2022-12-19 13:21:55 -0800

    Add space after variable declaration

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -9,6 +9,7 @@
 void swap_int(int *a, int *b)
 {
 	int t; /* create integer variable */
+
 	t = *a; /* assign value of *a to t */
 	*a = *b; /* assign value of *b to *a */
 	*b = t; /* assign value of t to *b */
