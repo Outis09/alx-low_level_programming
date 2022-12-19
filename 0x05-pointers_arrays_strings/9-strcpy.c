44fe80f9cdbf75eb803df99c57762e43222ef3da
Samuel Ayer <ayersamuel07@gmail.com>
2022-12-19 15:18:07 -0800

    Create file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies a string 
+ * @dest: points to a buffer
+ * @src: points to a string
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(*dest, *src);
+}
