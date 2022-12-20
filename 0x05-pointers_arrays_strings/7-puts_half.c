7c08558d7a848a6cbefcbd19ec4d6dea8251ddcb
Samuel Ayer <ayersamuel07@gmail.com>
2022-12-20 09:53:10 -0800

    Create program

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: string whose second half is to be printed
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len, n, i;
+
+	len = strlen(str);
+
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (len - 1) / 2;
+	}
+	for (i = n; i <= len - 1; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
