0af17236b82fd575094032ba23dd63d57075740c
Samuel Ayer <ayersamuel07@gmail.com>
2022-12-20 03:45:46 -0800

    Change function

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,5 +7,14 @@
  */
 void rev_string(char *s)
 {
-	strrev(s);
+	int i, len, tmp;
+
+	len = strlen(s);
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
+	}
 }
