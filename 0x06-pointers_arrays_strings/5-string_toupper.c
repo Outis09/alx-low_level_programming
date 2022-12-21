c7139aaf809117a6977a0cd24141409bc518fcbb
Samuel Ayer <ayersamuel07@gmail.com>
2022-12-21 11:11:30 -0800

    Create program

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * *string_toupper - convert all lowercase in a string to uppercase
+ *
+ * @*: pointer to string
+ * Return: all characters of the string in uppercase
+ */
+char *string_toupper(char *)
+{
+	char s;
+
+	s = toupper(*);
+
+	return (s);
+}
