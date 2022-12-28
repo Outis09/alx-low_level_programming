259a707e81e3d876b3831d24e7ed9f88e9248417
Samuel Ayer <ayersamuel07@gmail.com>
2022-12-28 08:19:12 -0800

    Create program

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.>
+
+/**
+ * main - prints alphabets in lowercase then uppercase
+ * followed by a new line
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	char s, a;
+
+	for (s = 'a'; s <= 'z'; s++)
+	{
+		putchar(s);
+	}
+	for (a = 'A'; a <= 'Z', a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+}
