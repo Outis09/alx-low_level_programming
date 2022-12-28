5eaa056b94c3c5f91e207602a08b3be0f56cc220
Samuel Ayer <ayersamuel07@gmail.com>
2022-12-28 09:58:03 -0800

    Create program

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits from 0 to 9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char s;
+
+	for (s = '0'; s <= '9'; s++)
+	{
+		putchar(s);
+	}
+	putcha('\n');
+
+	return (0);
+}
