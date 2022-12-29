a861cfa739c3126f94817f544136905f779eeb1b
Samuel Ayer <ayersamuel07@gmail.com>
2022-12-29 08:05:55 -0800

    Create program

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - print a string
+ * Return: zero (SUCCESS)
+ */
+int main(void)
+{
+	char var[] = "_putchar";
+
+	_putchar(var);
+	_putchar('\n');
+
+	return (0);
+}
