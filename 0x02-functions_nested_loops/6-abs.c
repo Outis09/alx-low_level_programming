feae557fb59e1acbb92dde42a8da183a1378c316
Samuel Ayer <ayersamuel07@gmail.com>
2022-12-30 04:20:13 -0800

    Create program

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - prints absolute value of an integer
+ * @int: integer
+ * Return: 0
+ */
+int _abs(int)
+{
+	int r;
+
+	abs(r);
+	return (0);
+}
