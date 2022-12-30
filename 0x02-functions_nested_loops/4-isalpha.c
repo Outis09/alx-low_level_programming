70104efc457d74c10df8e9b877dd6c97d8c5ab5f
Samuel Ayer <ayersamuel07@gmail.com>
2022-12-30 02:25:58 -0800

    Create program

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if a character is alphabetic
+ * @c: character to be checked
+ * Return: 1 if character is alphabetic,
+ * 0 if otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
