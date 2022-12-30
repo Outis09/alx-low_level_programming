859718f05557eeda35aa06b5517abb3508630362
Samuel Ayer <ayersamuel07@gmail.com>
2022-12-30 01:23:03 -0800

    Create program

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabets,
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char s;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (s = 'a'; s <= 'z'; s++)
+		{
+			_putchar(s);
+		}
+	}
+	_putchar('\n');
+}
