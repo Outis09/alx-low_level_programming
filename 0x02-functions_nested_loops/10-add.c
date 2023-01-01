875dcc474e4b2d578adcd577da82a815d240eafa
Samuel Ayer <ayersamuel07@gmail.com>
2023-01-01 10:57:43 -0800

    Create program

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two numbers and returns the results
+ * @i: number
+ * @x: number
+ * Return: results of addition
+ */
+int add(int i, int x)
+{
+	int a;
+
+	a = i + x;
+
+	return (a);
+}
