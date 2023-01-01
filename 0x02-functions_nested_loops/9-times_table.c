a0be858dfbf243be6e49fb265142c6a6304a2d90
Samuel Ayer <ayersamuel07@gmail.com>
2023-01-01 13:51:30 -0800

    Create program

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - prints the 9 times table
+ */
+void times_table(void)
+{
+	int j, k, x;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (k = 0; k <= 9; k++)
+		{
+			x = k * j;
+
+			printf("%d", x);
+			if (k != 9)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+		}
+}
