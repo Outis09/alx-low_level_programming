e389c9a0711294f02606dd54aefc1fae7932119c
Samuel Ayer <ayersamuel07@gmail.com>
2022-12-31 22:16:30 -0800

    Create program

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: number
+ * Return: last digit of n
+ */
+int print_last_digit(int n)
+{
+	int r;
+
+	if (n < 0)
+	{
+		r = -n % 10;
+		printf("%d", r);
+		return (r);
+	}
+	else 
+	{
+		r = n % 10;
+		printf("%d",r);
+		return (r);
+	}
+
+}
