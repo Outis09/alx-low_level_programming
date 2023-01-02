808d841f72f16bc9de79870845e0e2b37ac3ca12
Samuel Ayer <ayersamuel07@gmail.com>
2023-01-02 14:32:13 -0800

    Create program

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * strchr - locates a character in a string
+ * @s: string
+ * @c: character
+ * Return: pointer to the first occurrence of the character,
+ * Null if the string is not found
+ */
+char *strchar(char *s, char c)
+{
+	strchr(s, c);
+
+	return (s);
+}
