c3008d0002563c3230d8444d400cfcd483db3d31
Samuel Ayer <ayersamuel07@gmail.com>
2023-01-02 13:17:33 -0800

    Create program

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_memset - fills memmory with a constant byte
+ * @s: pointer to a memory area
+ * @b: constant byte
+ * @n: number of bytes to be filled
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b; unsigned int n)
+{
+	char *c;
+
+	c = memset(s, b, n);
+
+	return (c);
+}
