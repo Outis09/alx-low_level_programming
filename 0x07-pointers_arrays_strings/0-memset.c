e5d67aa983491fddc6671349b66a5d7468ef11c1
Samuel Ayer <ayersamuel07@gmail.com>
2023-01-02 13:26:28 -0800

    Replace 'memset' with for loop

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,15 +4,17 @@
 /**
  * *_memset - fills memmory with a constant byte
  * @s: pointer to a memory area
- * @b: constant byte
  * @n: number of bytes to be filled
  * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b; unsigned int n)
 {
-	char *c;
+	unsigned int i;
 
-	c = memset(s, b, n);
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
 
-	return (c);
+	return (s);
 }
