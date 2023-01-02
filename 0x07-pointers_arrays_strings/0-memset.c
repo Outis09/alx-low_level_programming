c9effa8d71f890a42f03d66c1c1d3640d7249a34
Samuel Ayer <ayersamuel07@gmail.com>
2023-01-02 14:04:54 -0800

    Add description for b

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,6 +4,7 @@
 /**
  * *_memset - fills memmory with a constant byte
  * @s: pointer to a memory area
+ * @b: constant byte
  * @n: number of bytes to be filled
  * Return: pointer to the memory area s
  */
