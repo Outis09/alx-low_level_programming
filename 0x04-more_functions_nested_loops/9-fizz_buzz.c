25cb9f1dee2dbd11b79eea95ea0747ade6f79c80
Samuel Ayer <ayersamuel07@gmail.com>
2023-01-09 09:19:21 -0800

    Rename program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100
+ * followed by a new line,
+ * for multiples of 3, Fizz is printed,
+ * for multiples of 5, Buzz is printed,
+ * for multiples of both 3 and 5, FizzBuzz is printed
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 15) == 0)
+		{
+			printf("Fizzbuzz");
+			printf(" ");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+		else
+		{
+			printf("%d", i);
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
