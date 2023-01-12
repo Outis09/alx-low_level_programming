166c9434d50617e7493a7004b761ffecf351fb56
Samuel Ayer <ayersamuel07@gmail.com>
2023-01-12 05:26:37 -0800

    Change variable type

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,10 +3,11 @@
 /**
  * malloc_checked - allocates memory using malloc
  * @b: number of bytes of memory to be assigned
+ * Return: void pointer to allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
-	char *ptr;
+	void *ptr;
 
 	ptr = malloc(b);
 
